3107f5d01259d9ac3f26f79e582486ed10dd8d9d
Kenthouky <dollarskenthouky@gmail.com>
2022-08-22 21:36:09 +0100

    Edges

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = word[2]
+word_last_2 = word[-4]
+first_word, *middle_word, last_word = word.split()
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
